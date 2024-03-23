4f99c1fe85bdf653519c2b60105013f2962edb13
JerickPH <jerickpalerph23@gmail.com>
2024-03-23 07:37:36 +0100

    data changes

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,8 +1,8 @@
 export const contactsData = {
-    email: 'abusaid7388@gmail.com',
-    phone: '+8801608797655',
-    address: 'Middle Badda, Dhaka, Bangladesh - 1212 ',
-    github: 'https://github.com/said7388',
+    email: 'jerickpalerph23@gmail.com',
+    phone: '+639700664922',
+    address: 'Block 10, Lot 21, Deca Mulig, Toril, Davao City, Davao del Sur',
+    github: 'https://github.com/JerickPH',
     facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
     twitter: 'https://twitter.com/said7388',
